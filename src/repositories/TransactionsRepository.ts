7c0be1c250892f59fbb038e0175769f1a404e9bc
Marcelo Candido <marcelojose_candido@hotmail.com>
2020-04-15 12:31:21 -0300

    Desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acc, obj) => {
+      if (obj.type === 'income') {
+        acc += obj.value;
+      }
+      return acc;
+    }, 0);
+    const outcome = this.transactions.reduce((acc, obj) => {
+      if (obj.type === 'outcome') {
+        acc += obj.value;
+      }
+      return acc;
+    }, 0);
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type
+    });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
